56d38cbbee9c495f2e75ac1131810551b4258a91
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-20 23:10:07 +0800

    Install bulma checkradio extension

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,52 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
 
-@section('content')
+<section class="section">
+    <div class="container">
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <h1 class="title is-1">Sign In.</h1>
+                <div class="card">
+                    <div class="card-content">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="field">
+                                <label class="label">Email</label>
+                                <div class="control">
+                                    <input type="text" class="input {{ $errors->has('email') ? ' is-danger' : '' }}" name="email">
+                                </div>
+                                @if ($errors->has('email'))
+                                <p class="help is-danger">{{ $errors->first('email') }}</p>
+                                @endif
+                            </div>
+
+                            <div class="field">
+                                <label class="label">Password</label>
+                                <div class="control">
+                                    <input type="password" class="input {{ $errors->has('password') ? ' is-danger' : '' }}" name="password">
+                                </div>
+                                @if ($errors->has('password'))
+                                <p class="help is-danger">{{ $errors->first('password') }}</p>
+                                @endif
+                            </div>
+
+                            <div class="field">
+                                <input class="is-checkradio" id="exampleCheckboxNormal" type="checkbox" name="exampleCheckboxNormal" checked="checked">
+                                <label class="mx-0" for="exampleCheckboxNormal">Checkbox</label>
+                            </div>
+
+                            <div class="field">
+                                <button class="button is-primary">Sign In</button>
+                                <a href="{{ route('password.request') }}" class="button is-outlined is-pulled-right">Forgot password?</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -15,12 +61,11 @@
                             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"
+                                    required> @if ($errors->has('email'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
@@ -29,12 +74,10 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
+                                <input id="password" type="password" class="form-control" name="password" required> @if ($errors->has('password'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
                                 @endif
                             </div>
                         </div>
@@ -43,7 +86,7 @@
                             <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                        <input type="checkbox" name="remember" {{ old( 'remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
