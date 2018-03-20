01c3d7bb877ff9a360533dee156b361b13113175
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-20 19:40:24 +0800

    Update route

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ Auth::routes();
 Route::get('/', 'PostsController@index');
 Route::post('/favorite/{post}', 'PostsController@favoritePost');
 Route::post('/unfavorite/{post}', 'PostsController@unFavoritePost');
-Route::get('/users/favorites', 'UsersController@myFavorites')->middleware('auth');
+Route::get('/users/favorites', 'UsersController@myFavorites')->middleware('auth')->name('myfavorite');
