0699a88605633156225dcc256c6301180cc916bd
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-20 19:42:13 +0800

    Add bulma navbar js

--- public/js/app.js
+++ public/js/app.js
@@ -13988,6 +13988,26 @@ var app = new __WEBPACK_IMPORTED_MODULE_0_vue___default.a({
     el: '#app'
 });
 
+// Bulma responsive navbar
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                // Get the target from the "data-target" attribute
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+});
+
 /***/ }),
 /* 15 */
 /***/ (function(module, exports, __webpack_require__) {
